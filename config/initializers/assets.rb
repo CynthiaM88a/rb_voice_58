4c23f60b1d302f323f71d5cf540a7cac83ec2745
andresgmdmc <hgonzalez.dmc@gmail.com>
2017-02-24 17:18:09 -0500

    Usuarios e Inicio Sesión

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( carousel.css )
 Rails.application.config.assets.precompile += %w( holder.min.js )
+Rails.application.config.assets.precompile += %w( validator.min.js )
